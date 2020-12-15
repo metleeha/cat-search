f2e40ab85f521cd86e7ebd8194766441eb4e8ff8
metleeha <nodongth@gmail.com>
2020-12-15 21:33:40 +0900

    Refactoring

--- src/api/theDogAPI.js
+++ src/api/theDogAPI.js
@@ -1,4 +1,3 @@
-
 const API_ENDPOINT = 'https://api.thedogapi.com/v1';
 
 const request = async (url) => {
@@ -11,21 +10,17 @@ const request = async (url) => {
 };
 
 const api = {
-    fetchImage: async keyword => {
+    fetchDogs: async keyword => {
         const breeds = (await api.searchBreedByName(keyword)).map(breed => { return breed.id; });
-        const requests = breeds.map(breed => {
-            return request(`${API_ENDPOINT}/images/search?limit=50&breed_ids=${breed}`);
-        });
+        const requests = breeds.map(id => { return request(`${API_ENDPOINT}/images/search?limit=50&breed_ids=${id}`); });
 
         return Promise.all(requests).then(responses => {
             let result = [];
-            responses.forEach(response => {
-                result = result.concat(response);
-            });
+            responses.forEach(response => { result = result.concat(response); });
             return result;
         });
     },
-    fetchImageAll: () => {
+    fetchRandomDogs: () => {
         return request(`${API_ENDPOINT}/images/search?limit=50`);
     },
     searchBreedByName: keyword => {
@@ -33,4 +28,4 @@ const api = {
     }
 };
 
-export { api }; 
+export { api };
---
+++ src/components/Card.js
@@ -0,0 +1,43 @@
+export default class Card {
+    constructor({$target, data, onClick}) {
+        this.onClick = onClick;
+        this.data = data;
+        this.card = document.createElement('article');
+        this.card.className = 'dog-card';
+
+        $target.appendChild(this.card);
+
+        this.render();
+    }
+
+    getDetail() {
+        this.onClick(this.data);
+    }
+
+    render() {
+        const url = this.data.url;
+        const {name, origin} = this.data.breeds.length > 0 ? this.data.breeds[0] : { name: '정보없음', origin: '정보없음'};
+
+        const cardImage = document.createElement('img');
+        cardImage.className = 'card-image';
+        cardImage.src = url;
+
+        const cardInfo = document.createElement('article');
+        cardInfo.className = 'card-info';
+
+        const dogName = document.createElement('p');
+        dogName.className = 'dog-name';
+        dogName.innerText = name;
+
+        const dogOrigin = document.createElement('p');
+        dogOrigin.className = 'dog-origin';
+        dogOrigin.innerText = origin;
+
+        this.card.addEventListener('click', () => { this.getDetail(); });
+
+        cardInfo.appendChild(dogName);
+        cardInfo.appendChild(dogOrigin);
+        this.card.appendChild(cardImage);
+        this.card.appendChild(cardInfo);
+    }
+}
