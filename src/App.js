a9250859976e8e219f7c90f0bd22e5fbcaf271d9
metleeha <nodongth@gmail.com>
2020-12-15 09:50:27 +0900

    Feature: Search images by name of breed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import SearchBar from './components/SearchBar.js';
+import SearchResult from './components/SearchResult.js'
+
 export default class App {
     constructor() {
         console.log("App is created!");
 
-        
+        const body = document.body;
+
+        const searchBar = new searchBar(body);
+        const searchResult = new searchResult(body, []);
     }
 }
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,11 +1,32 @@
 export default class SearchResult {
-    constructor() {
+    constructor($target) {
+        this.$target = $target;
+        this.data = data;
+        this.data = [];
 
-
+        this.render();
     }
 
-    render () {
+    updateData(data) {
+        this.data = data;
+        this.render();
+    }
 
+
+    render () {
+        this.$target.innerHTML = '';
+
+        const itemGroupWrapper = document.createElement('div');
+        itemGroupWrapper.className = 'wrapper';
+
+        const itemGroup = document.createElement('div');
+        itemGroup.className = 'item-group';
+        this.data.map(cat => {
+            new Item(itemGroup, cat);
+        });
+
+        itemGroupWrapper.appendChild(itemGroup);
+        this.$target.appendChild(itemGroupWrapper);
         
     }
 }
